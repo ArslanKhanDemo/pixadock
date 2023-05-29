5179ff71b87347774aebf83c91ac077aa70fba36
Arslan_Azhar <arslankhandemo@gmail.com>
2023-05-29 18:19:42 +0500

    first commit

---
+++ src/app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+require("dotenv").config();
+let PORT = process.env.PORT || 3000;
+let HOST = process.env.HOST || "Local Host";
+
+
+
+
+
+
+app.listen(PORT,()=>{
+    console.log(`Server is connected to ${HOST} on PORT:${PORT}`);
+})
